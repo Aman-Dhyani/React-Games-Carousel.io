8c0b702a47c0d8fbe144cf088a132d77004f496a
Aman Dhyani <102688642+Aman-Dhyani@users.noreply.github.com>
2022-10-12 23:33:16 -0700

    Update Puzzle.jsx

--- src/Component/Category/Puzzle/Puzzle.jsx
+++ src/Component/Category/Puzzle/Puzzle.jsx
@@ -2,20 +2,28 @@ import React from 'react'
 import Card from '../FlipCard/Card'
 import data from '../json/GamesData.json'
 
-export default function Puzzle(props) {
+const Puzzle = (props) => {
 
-    const  PuzzleGames = data.Games[4].Puzzle
+    const PuzzleGames = data.Games[4].Puzzle
 
 
     return (
         <>
             <h1 className='categoryHeader'>Sports</h1>
             {
-           PuzzleGames.map(games => {
-                    return <Card key={games.id} showFrame={props.showFrame} image={games.image} name={games.name} summary={games.summary} source={games.source} url={games.url} />
+                PuzzleGames.map(games => {
+                    return <Card
+                        key={games.id}
+                        showFrame={props.showFrame}
+                        image={games.image}
+                        name={games.name}
+                        summary={games.summary}
+                        source={games.source}
+                        url={games.url} />
                 })
             }
         </>
     )
 }
 
+export default React.memo(Puzzle)
