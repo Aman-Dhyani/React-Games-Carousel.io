2b2b88a27ecfbc9d2472f4ee4c3b5ba1f6d7e07f
Aman Dhyani <102688642+Aman-Dhyani@users.noreply.github.com>
2022-10-12 23:32:20 -0700

    Update Action.jsx

--- src/Component/Category/Action/Action.jsx
+++ src/Component/Category/Action/Action.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Card from '../FlipCard/Card'
 import data from '../json/GamesData.json'
 
-export default function Action(props) {
+const Action = (props) => {
 
     const ActionGames = data.Games[1].Action
 
@@ -12,10 +12,19 @@ export default function Action(props) {
             <h1 className='categoryHeader'>Action</h1>
             {
                 ActionGames.map(games => {
-                    return <Card key={games.id} showFrame={props.showFrame} image={games.image} name={games.name} summary={games.summary} source={games.source} url={games.url} />
+                    return <Card
+                        key={games.id}
+                        showFrame={props.showFrame}
+                        image={games.image}
+                        name={games.name}
+                        summary={games.summary}
+                        source={games.source}
+                        url={games.url} />
                 })
             }
         </>
     )
 }
+
+export default React.memo(Action)
 
