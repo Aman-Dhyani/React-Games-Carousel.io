cfd38fa27886fea48bf571552efdb00038a1a66e
Aman Dhyani <102688642+Aman-Dhyani@users.noreply.github.com>
2022-10-12 23:32:50 -0700

    Update MyCreated.jsx

--- src/Component/Category/Mycreated/MyCreated.jsx
+++ src/Component/Category/Mycreated/MyCreated.jsx
@@ -3,7 +3,7 @@ import Card from '../FlipCard/Card'
 import data from '../json/GamesData.json'
 
 
-export default function MyCreated(props) {
+const MyCreated = (props) => {
 
     const MyCreatedGames = data.Games[0].MyCreated
 
@@ -13,10 +13,18 @@ export default function MyCreated(props) {
             <h1 className='categoryHeader'>My Created Games</h1>
             {
                 MyCreatedGames.map(games => {
-                    return <Card key={games.id}  showFrame={props.showFrame} image={games.image} name={games.name} summary={games.summary} source={games.source} url={games.url} />
+                    return <Card
+                        key={games.id}
+                        showFrame={props.showFrame}
+                        image={games.image}
+                        name={games.name}
+                        summary={games.summary}
+                        source={games.source}
+                        url={games.url} />
                 })
             }
         </>
     )
 }
 
+export default React.memo(MyCreated)
